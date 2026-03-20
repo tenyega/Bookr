6cacf5d9cdae5b0be77ad8e4f83bbe3844a9b8ab
tenyega <103499706+tenyega@users.noreply.github.com>
2026-03-20 13:47:54 +0100

    Update DJANGO_SETTINGS_MODULE for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookOnlin.settings')
 
 application = get_wsgi_application()
 
